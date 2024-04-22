db198967a3357fe64dfad486000862193b878ae5
Mingyu Li <lmyannie60@163.com>
2024-04-22 23:23:20 +0800

    Add existing project files prior to the push to GitHub.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import StartPage from './components/StartPage';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hi this is test</div>
+      <StartPage />
+    </div>
+  );
+}
+
+export default App;
